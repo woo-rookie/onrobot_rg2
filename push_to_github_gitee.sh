3e01aa6ffa0e6f749027c51fc4b4995681088d52
wangxin <1572914362@qq.com>
2020-11-12 19:36:31 +0800

    Delete push_to_github_gitee.sh

--- push_to_github_gitee.sh
+++
@@ -1,9 +0,0 @@
-git init
-git add .
-git remote add gitee https://gitee.com/woo-rookie/onrobot_rg2.git
-git remote add github https://github.com/woo-rookie/onrobot_rg2.git
-
-
-git commit -m "$(date)"
-git push gitee master
-git push github main
